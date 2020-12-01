33ec1ae7448fc781ae550e550bb92a1759e38913
Simone <simone.pagotto.kg@gmail.com>
2020-12-01 15:45:18 +0100

    caricamento

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 const myDischi = new Vue ({
 	el:"#root",
 	data:{
-		cds:[{'a':2,'b':3}]
+		cds:[]
 	},
 	mounted(){
 		axios
 			.get("https://flynn.boolean.careers/exercises/api/array/music")
-			.then(function(result) {
+			.then((result) => {
 				for(let i=0; i<result.data.response.length; i++){
 					
-				
 					this.cds.push(result.data.response[i]);
 				}
 			})
